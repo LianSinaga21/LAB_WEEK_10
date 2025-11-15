d4475fb78bd7bb166473eba04e3f101fb76e905a
LianSinaga21 <parsauliansinaga17@gmail.com>
2025-11-15 22:37:46 +0700

    Commit No. 2: Implement LiveData to Sync Activity and Fragment UI

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -27,12 +27,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun prepareViewModel() {
 
-        viewModel.total.observe(this) { total ->
-            updateText(total)
+        viewModel.total.observe(this) {
+            updateText(it)
         }
 
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
     }
+
 }
--- app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
+++ app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
@@ -6,10 +6,16 @@ import androidx.lifecycle.ViewModel
 
 class TotalViewModel : ViewModel() {
 
-    private val _total = MutableLiveData<Int>(0)
-    val total: LiveData<Int> get() = _total
+    // LiveData
+    private val _total = MutableLiveData<Int>()
+    val total: LiveData<Int> = _total
+
+    init {
+        // Nilai awal
+        _total.postValue(0)
+    }
 
     fun incrementTotal() {
-        _total.value = (_total.value ?: 0) + 1
+        _total.postValue((_total.value ?: 0) + 1)
     }
 }
